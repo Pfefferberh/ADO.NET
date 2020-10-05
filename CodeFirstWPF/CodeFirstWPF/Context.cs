14d2e4d2b32c38dbc3a802fd7695f481da9d6b95
unknown <pfefferberhorion@gmail.com>
2020-10-05 22:38:47 +0300

    done

---
+++ CodeFirstWPF/CodeFirstWPF/Context.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeFirstWPF
+{
+    using CodeFirst.Model;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Context : DbContext
+    {
+        public Context()
+           : base("name=Context")
+        {
+            Database.SetInitializer<Context>(new Initializer());
+        }
+        public virtual DbSet<Addres> Addres { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Manufacturer> Manufacturers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+
+}
---
+++ CodeFirstWPF/CodeFirstWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using CodeFirst.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows;
+
+namespace CodeFirstWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Context context = new Context();
+
+            var pr = new List<Product>()
+            {
+                new Product {Name = "IPhone" , Price = 320, Category = context.Categories.FirstOrDefault(x=>x.Name=="phones")},
+                new Product {Name = "Meat" , Price = 30, Category = context.Categories.FirstOrDefault(x=>x.Name=="food")},
+                new Product {Name = "vodlka" , Price = 320, Category = context.Categories.FirstOrDefault(x=>x.Name=="phones")},
+                new Product {Name = "samsung" , Price = 320, Category = context.Categories.FirstOrDefault(x=>x.Name=="alcohol")},
+                new Product {Name = "xiaomi" , Price = 320, Category = context.Categories.FirstOrDefault(x=>x.Name=="phones")},
+            };
+            context.Products.AddRange(pr);
+            context.SaveChanges();
+
+            context.Addres.Add(new Addres() 
+            { Country = "Ukrain", City = "Rivne", Street = "Olesya 12" });
+            context.SaveChanges();
+
+            var cl = new List<Client>();
+            cl.Add(new Client(){ Name = "Vasya" });
+            context.Clients.AddRange(cl);
+            context.SaveChanges();
+
+            var or = new List<Order>();
+            or.Add(new Order{ProductId=1, AdressId=1,ClientId=1,Date=DateTime.Now});
+            context.Orders.AddRange(or);
+            context.SaveChanges();
+
+            dgclient.ItemsSource = cl;
+            dgorder.ItemsSource = or;
+            dgproduct.ItemsSource =pr;
+        }
+    }
+}
